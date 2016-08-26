96eebeaea2439471b20ac8342e5ab7b4db6d0019
Gery Debongnie <gery.debongnie@gmail.com>
2016-08-26 07:08:34 +0200

    update demo data

--- tests/dataLoading.test.js
+++ tests/dataLoading.test.js
@@ -6,7 +6,7 @@ const TextDB = require('../src/textdb')
 
 describe('basic loading operations', function () {
   it('db should load demo data', function () {
-    const db = new TextDB({path: __dirname + '/demo'})
+    const db = new TextDB({path: __dirname + '/../example'})
     const accounts = db.select('accounts')
 
     expect(accounts.length).to.be.equal(4)
